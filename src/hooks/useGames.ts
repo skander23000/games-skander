df006be8cee5bb0844c25610b0ebfb5de2ea8976
skander23000 <skanderchouiter@gmail.com>
2024-01-05 21:58:18 +0100

    Refactoring fetch-games with React-Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import apiClient from "../services/api-client";
+import { fetchResponse } from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -16,16 +18,19 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<fetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<fetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
+
 export default useGames;
--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,5 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import { fetchResponse } from "../services/api-client";
+import apiClient from "../services/api-client";
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<fetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
