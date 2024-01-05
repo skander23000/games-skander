f85162353c9dac3049a2609f6911b7c336485179
skander23000 <skanderchouiter@gmail.com>
2024-01-05 22:26:31 +0100

    Deleting duplicate interfaces

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -3,7 +3,7 @@ import platforms from "../data/platforms";
 import { fetchResponse } from "../services/api-client";
 import apiClient from "../services/api-client";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
