d1e38cf2d2b1a4de9c037782bb990ee0d4a49c67
skander23000 <skanderchouiter@gmail.com>
2024-01-06 14:06:12 +0100

    Implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { fetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatform";
+import { all } from "axios";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -14,17 +15,21 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<fetchResponse<Game>, Error>({
+  useInfiniteQuery<fetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
   });
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 
 export interface fetchResponse<T> {
   count: number;
+  next: string | null;
   results: T[];
 }
 
